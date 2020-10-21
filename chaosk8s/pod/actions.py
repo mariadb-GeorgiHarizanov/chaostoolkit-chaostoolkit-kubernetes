fc9b491576291fc835235ba3ed7eb3d540ebbc92
Georgi Dimitrov <georgi.harizanov@mariadb.com>
2020-10-21 12:20:38 +0300

    added kill main process function

--- chaosk8s/pod/actions.py
+++ chaosk8s/pod/actions.py
@@ -17,7 +17,120 @@ from logzero import logger
 
 from chaosk8s import create_k8s_api_client
 
-__all__ = ["terminate_pods", "exec_in_pods", "delete_pods"]
+__all__ = ["kill_main_process", "terminate_pods", "exec_in_pods", "delete_pods"]
+
+
+
+def kill_main_process(label_selector: str = None, name_pattern:
+                      str = None, all: bool = False, rand:
+                      bool = False, mode: str = "fixed", qty: int = 1,
+                      ns: str = "default", order: str = "alphabetic",
+                      container_name: str = "*", signal: str = "SIGTERM",
+                      pumba_image: str = "gaiaadm/pumba:master",
+                      wait_time: int = 35, secrets: Secrets = None):
+    """
+    Kill the main process in a pod's container. Select the appropriate pods
+    by label and/or name patterns. Whenever a pattern is provided for the
+    name, all pods retrieved will be filtered out if their name do not match
+    the given pattern.
+    If neither `label_selector` nor `name_pattern` are provided, all pods
+    in the namespace will be selected to kill their container's main process.
+    If `all` is set to `True`, all matching pods will terminate ther main
+    containers processes.
+    If `containe_name` is set to `*` all main processes in all containers of
+    the pod will be killed. Otherwise, only the containers that match the
+    given name in a pod.
+    The parameter `signal` defines which kill signal is sent to the pod's main
+    container. By default a `SIGTERM` signal is sent.
+    The parameter `ns` defines the namespace in which pods are being selected.
+    By default the `default` namespace is used.
+    Value of `qty` varies based on `mode`.
+    If `mode` is set to `fixed`, then `qty` refers to number of pods that kill
+    their container's main processes. If `mode` is set to `percentage`, then
+    `qty` refers to percentage of pods, from 1 to 100, that kill their
+    container's main processes. Default `mode` is `fixed` and default `qty`
+    is `1`.
+    If `order` is set to `oldest`, the retrieved pods will be ordered
+    by the pods creation_timestamp, with the oldest pod first in list.
+    If `rand` is set to `True`, n random pods will be chosen to kill their
+    container's main processes. Otherwise, the first retrieved n pods
+    be chosen to kill their container's main process.
+    With the parameter `pumba_image` one can change the image of pumba to
+    delete processes. It defaults to the current master of the project.
+    The parameter `wait_time` defines the amount of seconds (default `35`),
+    that is waited until the pumba pods are checked for correct execution.
+    """
+
+    # determine the pods to kill
+    pods_to_kill = _select_pods(label_selector=label_selector,
+                                name_pattern=name_pattern,
+                                all=all, rand=rand,
+                                mode=mode, qty=qty,
+                                ns=ns, order=order,
+                                secrets=secrets)
+
+    # initiate pumba pods on the nodes which accommodate the pods whose
+    # processes to kill
+    api = create_k8s_api_client(secrets)
+
+    v1 = client.CoreV1Api(api)
+
+    i = 0
+    timestamp = int(time.time())
+    for pod in pods_to_kill:
+        pumba_pod = client.V1Pod()
+        pumba_pod.metadata = client.V1ObjectMeta(name="pumba-pod-%d-%d" %
+                                                 (timestamp, i))
+        pumba_pod.metadata.labels = {
+            "app": "pumba",
+            "com.gaiaadm.pumba": "true",
+            "container": container_name,
+            "pod": pod.metadata.name,
+            "namespace": ns
+        }
+        container = client.V1Container(name="pumba")
+        container.image = pumba_image
+        container.image_pull_policy = "Always"
+        container.args = ["--log-level", "debug", "kill", "--signal", signal,
+                          "re2:^k8s_%s_%s_%s" % (container_name,
+                                                 pod.metadata.name, ns)]
+        resources = client.V1ResourceRequirements(
+            requests={
+                    "cpu": "10m",
+                    "memory": "5M"
+                },
+            limits={
+                    "cpu": "100m",
+                    "memory": "20M"
+                })
+        container.resources = resources
+        volume_mount = client.V1VolumeMount(mount_path="/var/run/docker.sock",
+                                            name="dockersocket")
+        container.volume_mounts = [volume_mount]
+        spec = client.V1PodSpec(containers=[container])
+        spec.restart_policy = "Never"
+        spec.node_name = pod.spec.node_name
+        host_path = client.V1HostPathVolumeSource(path="/var/run/docker.sock")
+        volume = client.V1Volume(name="dockersocket", host_path=host_path)
+        spec.volumes = [volume]
+        pumba_pod.spec = spec
+        v1.create_namespaced_pod(ns, pumba_pod)
+        i += 1
+
+    time.sleep(wait_time)
+
+    # check every created pumba pod for correct execution and in case of no
+    # error clean it up
+    for j in range(0, i):
+        pumba_pod = v1.read_namespaced_pod("pumba-pod-%d-%d" % (timestamp, j),
+                                           ns)
+        if pumba_pod.status.phase == "Succeeded":
+            v1.delete_namespaced_pod("pumba-pod-%d-%d" % (timestamp, j), ns)
+        else:
+            logger.error("wasn't able to kill main process in container %s in\
+                          pod %s" % (pumba_pod.metadata.labels['container'],
+                                     pumba_pod.metadata.labels['pod']))
+            return False
 
 
 def terminate_pods(label_selector: str = None, name_pattern: str = None,
